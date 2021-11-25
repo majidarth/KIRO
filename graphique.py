372da96e62f91a3038bce76f49d3d6e860c21042
TheiloT <theilo.terrisse@gmail.com>
2021-11-25 17:40:20 +0100

    Improve graphique.py (again)

--- graphique.py
+++ graphique.py
@@ -10,33 +10,36 @@ class Node():
         self.id = id
         self.coords = coords
     def __repr__(self):
-        return f"{self.type}_{self.id}"
+        return f"{self.type}{self.id}"
 
 
-instance = lire_instances("Kiro-small.json")
-
-
-G = nx.Graph()
+instance = lire_instances("Kiro-large.json")
 
 # Ajout des noeuds
+
+G_clients = nx.Graph()
 
 pre_clients = instance["clients"]
 clients = [Node("I", client["id"], coords=client["coordinates"]) for client in pre_clients]
 clients_pos = {client:client.coords for client in clients}
 
-G.add_nodes_from(clients)
+G_clients.add_nodes_from(clients)
+
+G_sites = nx.Graph()
 
 pre_sites = instance["sites"]
 sites = [Node("S", site["id"], coords=site["coordinates"]) for site in pre_sites]
 sites_pos = {site:site.coords for site in sites}
 
-G.add_nodes_from(sites)
+G_sites.add_nodes_from(sites)
 
 # Tracé
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-pos = clients_pos
-pos.update(sites_pos)
-nx.draw(G, pos=pos,  with_labels = True, node_color='lightblue', ax = ax)
+# pos = clients_pos
+# pos.update(sites_pos)
+nx.draw(G_clients, pos=clients_pos,  with_labels = False, node_color='lightblue', ax = ax)
+nx.draw(G_sites, pos=sites_pos,  with_labels = True, node_color='red', ax = ax)
 
+print(len(sites))
 plt.show()
